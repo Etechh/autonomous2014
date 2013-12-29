0598b71735778f715dddce1f5c8f632b4dca88c7
Eva van Houten <etech@live.nl>
2013-12-29 19:57:40 +0100

    Changed pragma, changed variables to match prgmstyle tele-op, added motor controls to gyro function

--- Autonomous.c
+++ Autonomous.c
@@ -1,22 +1,17 @@
-#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTMotor,  HTServo)
+#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTServo,  HTMotor)
 #pragma config(Sensor, S1,     ,               sensorI2CMuxController)
-#pragma config(Sensor, S2,     GYRO,           sensorAnalogInactive)
-#pragma config(Sensor, S3,     IRSEEKER,       sensorI2CCustom)
+#pragma config(Sensor, S2,     gyro,           sensorAnalogInactive)
+#pragma config(Sensor, S3,     IR,             sensorI2CCustom)
 #pragma config(Motor,  motorA,          flag,          tmotorNXT, PIDControl, encoder)
 #pragma config(Motor,  motorB,          jawsr,         tmotorNXT, PIDControl, encoder)
 #pragma config(Motor,  motorC,          jawsl,         tmotorNXT, PIDControl, encoder)
-#pragma config(Motor,  mtr_S1_C1_1,     frontright,    tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C1_2,     backleft,      tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C2_1,     backright,     tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C2_2,     frontleft,     tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C3_1,     motorH,        tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C3_2,     motorI,        tmotorTetrix, openLoop)
-#pragma config(Servo,  srvo_S1_C4_1,    servo1,               tServoNone)
-#pragma config(Servo,  srvo_S1_C4_2,    servo2,               tServoNone)
-#pragma config(Servo,  srvo_S1_C4_3,    servo3,               tServoNone)
-#pragma config(Servo,  srvo_S1_C4_4,    servo4,               tServoNone)
-#pragma config(Servo,  srvo_S1_C4_5,    servo5,               tServoNone)
-#pragma config(Servo,  srvo_S1_C4_6,    servo6,               tServoNone)
+#pragma config(Motor,  mtr_S1_C1_1,     fr,            tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C1_2,     br,            tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C2_1,     lift,            tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C4_1,     bl,          tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C4_2,     fl,        tmotorTetrix, openLoop)
+#pragma config(Servo,  srvo_S1_C3_1,    arml,                 tServoStandard)
+#pragma config(Servo,  srvo_S1_C3_2,    armr,                 tServoStandard)
 
 #include "JoystickDriver.c"
 
@@ -24,53 +19,61 @@
 #include "hitechnic-gyro.h"
 #include "hitechnic-irseeker-v2.h"
 
-int TURNINGSPEED = 80;
-float HEADING;
+int turnspd = 80;
+float heading;
+
 
 void initializeRobot()
 {
-  HTGYROstartCal(GYRO); //Calibrate gyro sensor, make sure robot is still
+  HTGYROstartCal(gyro); //Calibrate gyro sensor, make sure robot is still
 
   return;
 }
 
+
 //Function for turning, use: turnto(heading goal for robot relative to starting position in degrees)
-void turnto(float HEADINGGOAL)
+void turnto(float headinggoal)
 {
-	float ROTSPEED = 0;
-	int MOTORSPEED = 0;
+	float rotspd = 0;
+	int motorspd = 0;
 
 	//Check which way should be turned and adjust motor direction
-	if(HEADING < HEADINGGOAL)
-	{MOTORSPEED = TURNINGSPEED;}
-	else if(HEADING > HEADINGGOAL)
-	{MOTORSPEED = -TURNINGSPEED;}
+	if(heading < headinggoal)
+	{motorspd = turnspd;}
+	else if(heading > headinggoal)
+	{motorspd = -turnspd;}
 
 	//Turn on motors
-	/* To be written */
+	motor[fl] = motorspd;
+	motor[fr] = motorspd;
+	motor[bl] = motorspd;
+	motor[br] = motorspd;
 
-	while(HEADING != HEADINGGOAL)
+	while(heading != headinggoal)
 	{
 		//This is where the motors actually turn, 1 ms at the time
 		wait1Msec(1);
 
 		//Get rotationspeed value from gyrosensor
-		ROTSPEED = HTGYROreadRot(GYRO);
+		rotspd = HTGYROreadRot(gyro);
 
 		//Jawohl, ein Riemannsumm fur die integrale!
-		HEADING += ROTSPEED * 0.001;
+		heading += rotspd * 0.001;
 
 		//Display heading on the display for testing
-		nxtDisplayCenteredBigTextLine(3, "%2.0f", HEADING);
+		nxtDisplayCenteredBigTextLine(3, "%2.0f", heading);
 	}
 
 	//Turn off motors when the while loop ends, so when the heading = the heading goal
-	/* To be written */
+	motor[fl] = 0;
+	motor[fr] = 0;
+	motor[bl] = 0;
+	motor[br] = 0;
 }
 
 task main()
 {
-  initializeRobot();
+  initializeRobot(); //Calibrate sensors
 
   waitForStart(); //Wait for the beginning of autonomous phase
 }
